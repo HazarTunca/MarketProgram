34e1d2c017573e7bc7accf9af1f943c60c15e098
HazarTunca <hazartunca@outlook.com>
2022-10-11 21:58:59 +0300

    timer using

--- MarketProgram/FormUtils/Utils/EventSuppressor.cs
+++ MarketProgram/FormUtils/Utils/EventSuppressor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace XIV.Utils
 {
