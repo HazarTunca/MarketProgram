ab3c9b5743288b01fd060d65d312059cc5278106
alimertcetin <mrtmert@gmail.com>
2022-10-11 21:17:22 +0300

    StoreInventory.cs

---
+++ MarketProgram/Store/StoreInventory.cs
@@ -0,0 +1,50 @@
+﻿using MarketProgram;
+using MarketProgram.SaveSystems;
+using System.Collections.Generic;
+
+namespace MarketProgram.Store
+{
+    public class StoreInventory : ISaveable
+    {
+        Dictionary<uint, ProductData> productDatas = new Dictionary<uint, ProductData>();
+
+        public void AddProduct(ProductData productData)
+        {
+            productDatas.Add(productData.barcodeID, productData);
+        }
+
+        public void AddProduct(uint barcodeID, string productName, double price)
+        {
+            productDatas.Add(barcodeID,
+                new ProductData
+                {
+                    barcodeID = barcodeID,
+                    productName = productName,
+                    price = price,
+                    count = 0
+                });
+        }
+
+        public void AddProduct(uint barcodeID, string productName, double price, uint count)
+        {
+            productDatas.Add(barcodeID,
+                new ProductData
+                {
+                    barcodeID = barcodeID,
+                    productName = productName,
+                    price = price,
+                    count = count,
+                });
+        }
+
+        public object GetSaveData()
+        {
+            return productDatas;
+        }
+
+        public void Load(object loadedData)
+        {
+            productDatas = (Dictionary<uint, ProductData>)loadedData;
+        }
+    }
+}
