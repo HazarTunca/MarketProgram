c3fe9164eafe69b4dc03cc7b305e9bac359f5e4b
alimertcetin <mrtmert@gmail.com>
2022-10-11 21:17:12 +0300

    ProductData.cs

---
+++ MarketProgram/SaveSystems/ProductData.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketProgram.SaveSystems
+{
+    [System.Serializable]
+    public class ProductData
+    {
+        public uint barcodeID;
+        public string productName;
+        public double price;
+        public uint count;
+    }
+}
