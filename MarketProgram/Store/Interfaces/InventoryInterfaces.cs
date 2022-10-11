37f49d2603d761d3ac10c5e42975a085253e6ad2
alimertcetin <mrtmert@gmail.com>
2022-10-11 23:22:53 +0300

    InventoryListeners

---
+++ MarketProgram/Store/Interfaces/InventoryInterfaces.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketProgram.Store.Interfaces
+{
+    public interface IInventoryChangedListener
+    {
+        public void OnInventoryChanged(StoreInventory storeInventory);
+    }
+
+    public interface IInventoryManager
+    {
+        public void AddItem(ProductData productData);
+    }
+
+}
