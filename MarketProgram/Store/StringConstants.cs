ccbec53784bd494bde6498ec8043831d75a1ecf8
alimertcetin <mrtmert54@gmail.com>
2022-10-21 00:04:25 +0300

    Created StringConstants

---
+++ MarketProgram/Store/StringConstants.cs
@@ -0,0 +1,16 @@
+﻿public static class StringConstants
+{
+    public const string Title_Error = "Hata";
+    public const string Error_Invalid_BarcodeID = "Geçersiz barkod numarası.";
+    public const string Error_Invalid_ProductName = "Geçersiz ürün ismi.";
+    public const string Error_Invalid_Price = "Geçersiz fiyat.";
+    public const string Error_Invalid_ItemAmount = "Geçersiz ürün miktarı.";
+
+    public const string Title_Warning = "Uyarı";
+    public const string Warning_ItemAlreadyExist = "Ürün daha önceden eklenmiş.";
+    public const string Warning_UpdateItem = "Var olan ürünü güncelle.";
+
+    public const string Title_Success = "Başarılı";
+    public const string Success_ItemAdded = "Ürün eklendi.";
+    public const string Success_ItemUpdated = "Ürün güncellendi.";
+}
