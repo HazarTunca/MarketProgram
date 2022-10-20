0f79118820d8ee8a9133a859ff639d1984244146
alimertcetin <mrtmert54@gmail.com>
2022-10-21 00:05:38 +0300

    ProductData - Initialize variables in constructor

--- MarketProgram/Store/ProductData.cs
+++ MarketProgram/Store/ProductData.cs
@@ -7,5 +7,18 @@
         public string productName;
         public double price;
         public uint count;
+
+        public ProductData()
+        {
+
+        }
+
+        public ProductData(string barcodeID, string productName, double price, uint count)
+        {
+            this.barcodeID = barcodeID;
+            this.productName = productName;
+            this.price = price;
+            this.count = count;
+        }
     }
 }
