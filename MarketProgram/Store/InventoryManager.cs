7cef705ad1f372d5d9abb75701e08f78be18e643
alimertcetin <mrtmert54@gmail.com>
2022-10-21 00:06:26 +0300

    InventoryManager renamed as MarketProgramPaths
    And correctly combined paths

--- MarketProgram/Store/InventoryManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using MarketProgram.Store.Interfaces;
-
-namespace MarketProgram.Store
-{
-    public static class InventoryManager
-    {
-        public static string productSavePath = Path.Combine(Application.UserAppDataPath, "MarketProgram/ProductSave");
-    }
-}
---
+++ MarketProgram/Store/MarketProgramPaths.cs
@@ -0,0 +1,10 @@
+﻿using MarketProgram.Store.Interfaces;
+
+namespace MarketProgram.Store
+{
+    public static class MarketProgramPaths
+    {
+        public static string BaseFolderPath = Path.Combine(Application.UserAppDataPath, "MarketProgram");
+        public static string ProductSavePath = Path.Combine(BaseFolderPath, "ProductSave");
+    }
+}
